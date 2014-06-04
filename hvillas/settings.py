5498185273946bf94701e216111eb95c29142b1b
hoho <the_h_flower@live.com>
2014-06-04 05:31:36 +0300

    fdf
    fsd

--- hvillas/settings.py
+++ hvillas/settings.py
@@ -104,6 +104,8 @@ LOGOUT_URL = '/'
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+@host/db')
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
